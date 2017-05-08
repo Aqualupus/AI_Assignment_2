f5ecfaeca06a0332c2e41d4c37cb89aa40552d51
meg <megabyte.lk@gmail.com>
2017-05-08 17:41:56 +1000

    Set up of testing with basic apple implies fruit test

---
+++ MegaTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AI_Assignment_2;
+
+namespace MegaTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BuildKB KB = new BuildKB("apple.txt");
+            TruthTable TT = new TruthTable(KB.Implies, KB.Vars, KB.TrueVars, "apple");
+            
+            Assert.IsTrue(TT.BuildTT() == "Yes 1", "Returns yes with 1 level for apple");
+        }
+    }
+}
